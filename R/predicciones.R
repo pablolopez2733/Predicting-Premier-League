fa72d92d24b55bbce95c8c12c696e787ab8aa27f
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-06-18 12:25:54 -0500

    Modifique la tabla de modelos

--- R/predicciones.R
+++ R/predicciones.R
@@ -25,6 +25,7 @@ summary(wsmod)
 url2<- 'https://github.com/pablolopez2733/Predicting-Premier-League/raw/master/CleanData/1920metrics.csv'
 premier19<-read.csv(url2)
 
+#Prediccion con xG
 xGvalues <- data.frame(xG = premier19$xG)
 predictionsxG <- predict(mod, newdata=xGvalues)
 
@@ -32,3 +33,14 @@ predicciones <- data.frame(as.list(predictionsxG))
 predicciones <- as.data.frame(t(predicciones))
 predicciones$Puntos = exp(predicciones$V1)
 
+finalxG <- cbind(Equipo = premier19[,1], predicciones)
+
+#Prediccion con wsRating
+wsr <- data.frame(wsRating = premier19$whoscored)
+predictionsws <- predict(wsmod, newdata=wsr)
+
+predicciones_ws <- data.frame(as.list(predictionsws))
+predicciones_ws <- as.data.frame(t(predicciones_ws))
+predicciones_ws$Puntos = exp(predicciones_ws$V1)
+
+final_ws <- cbind(Equipo = premier19[,1], predicciones_ws)
